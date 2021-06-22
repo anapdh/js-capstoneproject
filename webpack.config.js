e307c7b6330b05fa1b442dc6273126e780e218c7
anapdh <ana_d_hubner@hotmail.com>
2021-06-22 00:15:37 -0300

    Fix index.html to compile inside dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 const webpack = require('webpack');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
   entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/',
-    filename: 'project.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
   },
 
   module: {
     rules: [
       {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
 
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
     new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
   ],
-
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+  },
 };
